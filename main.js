8f7a086f52d297f6e59b21d74e82d31474b7bceb
Jhoan <jegarcia155@misena.edu.co>
2022-09-29 08:20:37 -0500

    Ejercicio 3

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 2. Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
-let num = Number(prompt(`Escriba un número`))
-let num1 = Number(prompt(`Escriba un segundo número`))
-let num2 = Number(prompt(`Escriba un tercer número`))
+// 3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o
+// si son iguales.
+let num1 = Number(prompt(`Escriba el valor 1`))
+let num2 = Number(prompt(`Escriba el valor 2`))
+let num3 = Number(prompt(`Escriba el valor 3`))
 
-if (num > num1 && num > num2) {
-    console.log(`El numero ${num} es el mayor`)
+if (num1 > num2 && num1 > num3) {
+    console.log(`El valor ${num1} es el mayor`)
 }
-if (num1 > num && num1 > num2) {
-    console.log(`El numero ${num1} es el mayor`)
+if (num1 < num2 && num1 < num3) {
+    console.log(`El valor ${num1} es el menor`)
 }
-else {
-    console.log(`El numero ${num2} es el mayor.`)
+if (num1 == num2) {
+    console.log(`El valor 1 es igual al valor 2`)
 }
+if (num1 == num3) {
+    console.log(`El valor 1 es igual al valor 3`)
+}
+if (num2 > num3 && num2 > num3){
+    console.log(`El valor ${num2} es el mayor`)
+}
+if (num2 < num3 && num2 < num1){
+    console.log(`El valor ${num2} es el menor`)
+}
+if (num2 == num3){
+    console.log(`El valor 2 es igual al valor 3`)
+}
+if (num3 > num1 && num3 > num2){
+    console.log(`El valor ${num3} es el mayor`)
+}
+if (num3 < num1 && num3 < num2){
+    console.log(`El valor ${num3} es el menor`)
+}
+
 })
