f331ea6b318ce4cdd2989d16cc16baf83abd5395
Jhoan <jegarcia155@misena.edu.co>
2022-09-29 08:00:35 -0500

    Ejercicio 2

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 addEventListener("DOMContentLoaded", ()=>{
-
+// 2. Calcule el mayor de tres números, permitiendo leer 3 valores diferentes
 let num = Number(prompt(`Escriba un número`))
+let num1 = Number(prompt(`Escriba un segundo número`))
+let num2 = Number(prompt(`Escriba un tercer número`))
 
-if (num > 0 && num <= 100) {
-    console.log(`El número  ${num} es positivo y menor o igual a 100`)
+if (num > num1 && num > num2) {
+    console.log(`El numero ${num} es el mayor`)
+}
+if (num1 > num && num1 > num2) {
+    console.log(`El numero ${num1} es el mayor`)
 }
 else {
-    console.log(`El número que ingreso es negativo o mayor a 100.`)
+    console.log(`El numero ${num2} es el mayor.`)
 }
 })
