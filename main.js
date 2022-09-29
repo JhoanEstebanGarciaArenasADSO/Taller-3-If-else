dea1ff8705ecaebbf807098297049e57cef86d0f
Jhoan Garcia <jegarcia155@misena.edu.co>
2022-09-29 16:41:53 -0500

    Ejercicio 13

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 addEventListener("DOMContentLoaded", ()=>{
     
-// 12. Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
-// caso contrario indicar con un mensaje que la operación no es posible realizarla
+// 13. En una tienda de HELADO da un descuento por compra a sus clientes con membresía dependiendo
+// Los descuentos son los siguientes:
+// de su tipo, sólo existen tres tipos de membresía, tipo A, tipo B y tipo C.
+// Tipo A 10% de descuento
+// Tipo B 15% de descuento
+// Tipo C 20% de descuento
+let mem, desc, prod
 
-let num1, num2, res
+mem= prompt("Ingrese su membresia, si es TipoA ingresa (a), si es TipoB ingresa (b), si es TipoC ingresa (c): ")
 
-num1= Number(prompt("Ingrese un numero: "))
-num2= Number(prompt("Ingrese otro numero: "))
-
-if (num1>num2){
-    res = num1 - num2
-    console.log(`El resultado de la resta es: ${res}`)
-    alert(`El resultado de la resta es: ${res}`)
+if (mem == "a"){
+    prod= Number(prompt("Ingrese el precio del producto: "))
+    desc= prod*0.10
+    console.log(`El descuento del producto es: ${desc}`)
+    alert(`El descuento del producto es: ${desc}`)
 }
-else{
-    console.log(`La operacion no es posible realizarla`)
-    alert(`La operacion no es posible realizarla`)
+if (mem == "b"){
+    prod= Number(prompt("Ingrese el precio del producto: "))
+    desc= prod*0.15
+    console.log(`El descuento del producto es: ${desc}`)
+    alert(`El descuento del producto es: ${desc}`)
+}
+if (mem == "c"){
+    prod= Number(prompt("Ingrese el precio del producto: "))
+    desc= prod*0.20
+    console.log(`El descuento del producto es: ${desc}`)
+    alert(`El descuento del producto es: ${desc}`)
 }
 })
