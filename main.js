07bc5e3623ea2cad267193473c4c7e8625e7c267
Jhoan Garcia <jegarcia155@misena.edu.co>
2022-09-29 16:32:27 -0500

    Ejercicio 12

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 addEventListener("DOMContentLoaded", ()=>{
     
-// 11. Programa para solicitar el nombre, apellido, edad y la nota promedio de 5 estudiantes de un curso
-// de computación
+// 12. Restarle al primer número al segundo (siempre y cuando el primero sea mayor que él segundo, en
+// caso contrario indicar con un mensaje que la operación no es posible realizarla
 
-let nom, apellido, edad, nota1, nota2, nota3, promed, i
-for (i=0; i < 5; i++) {
-    nom=prompt("Ingresa tus nombres: ")
-    apellido=prompt("Ingresa tus apellidos: ")
-    edad=Number(prompt("Ingresa tu edad: "))
-    nota1=Number(prompt("Ingresa la nota 1: "))
-    nota2=Number(prompt("Ingresa la nota 2: "))
-    nota3=Number(prompt("Ingresa la nota 3: "))
-    promed= (nota1+nota2+nota3)/3
-    console.log(`La nota promedio del estudiante  ${nom} ${apellido} es ${promed}`)
-    alert(`La nota promedio del estudiante  ${nom} ${apellido} es ${promed}`)
+let num1, num2, res
+
+num1= Number(prompt("Ingrese un numero: "))
+num2= Number(prompt("Ingrese otro numero: "))
+
+if (num1>num2){
+    res = num1 - num2
+    console.log(`El resultado de la resta es: ${res}`)
+    alert(`El resultado de la resta es: ${res}`)
 }
-
+else{
+    console.log(`La operacion no es posible realizarla`)
+    alert(`La operacion no es posible realizarla`)
+}
 })
