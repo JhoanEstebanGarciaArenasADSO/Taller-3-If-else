b4d61051ad640f2e5b0bac15ce3dfd4e2968f75e
Jhoan <jegarcia155@misena.edu.co>
2022-09-29 08:42:23 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,36 +1,21 @@
 addEventListener("DOMContentLoaded", ()=>{
-// 3. Desarrollar un diagrama que lea 3 valores diferentes e indique cual es el mayor de ellos, el menor o
-// si son iguales.
-let num1 = Number(prompt(`Escriba el valor 1`))
-let num2 = Number(prompt(`Escriba el valor 2`))
-let num3 = Number(prompt(`Escriba el valor 3`))
+// 4. Cálculo de áreas - Elige una figura geométrica:" Triángulo y Círculo
+// ¿Qué figura quiere calcular (Escriba T o C)?
+// Triangulo = base * altura / 2
+// Circulo = PI * radio* radio
 
-if (num1 > num2 && num1 > num3) {
-    console.log(`El valor ${num1} es el mayor`)
+let fig = prompt(`Elige la figura a la que le quiere calcular el area, si es Triangulo escriba (T) y si es circulo escriba(C)`)
+
+if (fig == 'T') {
+    let base = Number(prompt("Escribe la base del triangulo: "))
+    let altura = Number(prompt("Escribe la altura del triangulo: "))
+    let area = (base * altura)/2
+    console.log(`El area del triangulo es: ${area}`)
 }
-if (num1 < num2 && num1 < num3) {
-    console.log(`El valor ${num1} es el menor`)
-}
-if (num1 == num2) {
-    console.log(`El valor 1 es igual al valor 2`)
-}
-if (num1 == num3) {
-    console.log(`El valor 1 es igual al valor 3`)
-}
-if (num2 > num3 && num2 > num3){
-    console.log(`El valor ${num2} es el mayor`)
-}
-if (num2 < num3 && num2 < num1){
-    console.log(`El valor ${num2} es el menor`)
-}
-if (num2 == num3){
-    console.log(`El valor 2 es igual al valor 3`)
-}
-if (num3 > num1 && num3 > num2){
-    console.log(`El valor ${num3} es el mayor`)
-}
-if (num3 < num1 && num3 < num2){
-    console.log(`El valor ${num3} es el menor`)
+if (fig == 'C') {
+    let radio = Number(prompt("Escribe el radio del circulo: "))
+    let area = Math.PI * radio**2
+    console.log(`El area del circulo es: ${area}`)
 }
 
 })
